7c5c676384a4be3e361b50d5786a4181feba27a5
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-10-05 13:04:11 -0700

    added some notes

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-// import functions and grab DOM elements
+import { answers } from './answers.js';
 
-// initialize global state
-
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+const randomAnswer = getRandomNumber(answers.length);
+console.log(answers[randomAnswer]);
