2a98801068c00b35231d76d14913e44b085ac334
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-10-05 13:22:06 -0700

    wrote my answers in a different folder and got my index.html

--- answers.js
+++ answers.js
@@ -1,3 +1,9 @@
 export const answers = [
-    // answers go here
+    'As I see it, yes',
+    'Ask again later',
+    'Do not count on it',
+    'Ask again later',
+    'Better not tell you know',
+    'Cannot predict now'
+
 ];
